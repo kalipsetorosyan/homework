d02e36abf0f2ad1fd191eeffd89fd2b89eb692b6
kalipsetorosyan <kalipse.torosyan.g@gmail.com>
2024-02-29 00:18:19 +0400

    create given functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homework
+
+#this is my test
---
+++ functions.py
@@ -0,0 +1,61 @@
+# 1. Գրել ֆունկցիա, որը կվերդարձնի ստացած արգումենտներից թվերի
+# գումարը։
+
+# def count_func(*args):
+#     count = 0
+#     for el in args:
+#         count += el
+#     return count
+#
+# print(count_func(1,5,3,6))
+
+# 2. Գրել ֆունկցիա, որը կվերդարձնի ստացած արգումենտներից տողերի
+# քանակը։
+
+# def count_lines(text):
+#     lines = text.split("\n")
+#     return len(lines)
+# print(count_lines("hello\nworld\nejhr"))
+
+
+# 3. Գրել ֆունկցիա, որը կվերադարձնի ստասած արգումենտների միջին
+# թվաբանականը։
+# def average(*args):
+#     res = sum(args) / len(args)
+#     return res
+# print(average(1,2,3))
+
+# 4. Գրել ֆունկցիա, որը կստանա 2 արգումենտ և կվերադարձնի այդ
+# արգումենտերի հետ կատարած մաթեմատիկական գործողությունների
+# արդյունքները
+
+# def math_func(num1,num2,operator="+"):
+#     if operator == "+":
+#         return num1 + num2
+#     elif operator == "-":
+#         return num1 - num2
+#     elif operator == "*":
+#         return num1 * num2
+#     elif operator == "/":
+#         return num1 / num2
+#     else:
+#         return "operator is not defined"
+#
+# print(math_func(5,5,"*"))
+
+# 5. Գրել ֆունկցիա, որը որպես արգումենտ կստանա տողը և կվերադարձնի
+# այն դարձված ամբողջությամբ մեծատառ (upper ֆունկցիան օգտագործել չի
+# կարելի)։
+
+#
+# def my_upper(text):
+#     for el in text:
+#         if el.isalpha():
+#
+#         else:
+#             continue
+#
+#
+#     return text
+# print(my_upper("hell1"))
+# print("hell".isalpha())
